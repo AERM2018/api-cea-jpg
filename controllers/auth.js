0dd8babcdc80badb1111c25fc61513277ce4ac71
Angel Retana <retana.martinez.angel.eduardo@gmail.com>
2021-03-11 17:56:45 -0600

    auth fixed (sign up)

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,20 +4,57 @@ const { db } = require("../database/connection")
 const {getUserById} = require('../queries/queries');
 const { createJWT } = require("../helpers/jwt");
 const bcrypt = require('bcryptjs');
-const login = async( req, res = response ) => {
-    const { id, password} = req.body;
-    const user = await db.query(getUserById,{
-        replacements : {'id':id},
-        type : QueryTypes.SELECT});
+const User = require("../models/user");
 
-    if(user.length < 1){
-        return res.status(404).json({
-            ok : false,
-            msg : `El usuario con id ${id} no existe, verifiquelo por favor`
+const signup = async( req, res = response ) => {
+    const { id, user_type, email,  password} =  req.body
+    const salt = bcrypt.genSaltSync()
+    const passwordEncrypted = bcrypt.hashSync(password,salt)
+    const user = new User({user_type, email, password : passwordEncrypted})
+    await user.save()
+
+    res.status(201).json({
+        ok :true,
+        msg : "Admin creado correctamente"
+    })
+}
+const login = async( req, res = response ) => {
+    let user;
+    let passValidation;
+    const { id, password } = req.body;
+    if( id === 'admin'){
+        user = await User.findOne( {
+            where : {'user_type' : 'admin'}
         })
+
+        if(!user){
+            return res.status(404).json({
+                ok : false,
+                msg : 'No se encontró a ningun usuario administrador'
+            })
+        }
+
+        user.id_role = 1
+        passValidation = bcrypt.compareSync(password,user.password);
+
+
+    }else{
+        const normalUser = await db.query(getUserById,{
+            replacements : {'id':id},
+            type : QueryTypes.SELECT});
+        
+        user = normalUser[0]
+    
+        if(!user){
+            return res.status(404).json({
+                ok : false,
+                msg : `El usuario con id ${id} no existe, verifiquelo por favor`
+            })
+        }
+        passValidation = bcrypt.compareSync(password, user.password)
     }
-    const verifyPassword = bcrypt.compareSync(password, user[0].password)
-    if(!verifyPassword){
+
+    if(!passValidation){
         return res.status(400).json({
             ok : false,
             msg : `Datos de acceso erroneos, verifiquelos por favor.`
@@ -25,7 +62,7 @@ const login = async( req, res = response ) => {
     }
     
     // get data and create token
-    const {id_user,user_type, id_role} = user[0]
+    const {id_user,user_type, id_role} = user
     const token = await createJWT(id_user,user_type,id_role)
 
     res.status(200).json({
@@ -57,5 +94,6 @@ const revalidateJWT = async( req, res = response) => {
 
 module.exports = {
     login,
-    revalidateJWT
+    revalidateJWT,
+    signup
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 const {Router} = require('express');
-const { login, revalidateJWT } = require('../controllers/auth');
+const { login, revalidateJWT, signup } = require('../controllers/auth');
 const validateJWT = require('../middlewares/validar-jwt');
 
 const authRouter = Router();
 
 authRouter.get('/login', login)
 authRouter.get('/renew', validateJWT, revalidateJWT)
+authRouter.post('/signup',signup)
 
 
 module.exports = authRouter;
