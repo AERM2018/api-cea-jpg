fcf46ead49dcdeebbf5a68d98af9dd8a9b9b297e
AERM2018 <retana.martinez.angel.eduardo@gmail.com>
2022-04-23 13:17:24 -0500

    Done: Return test a student can take

--- controllers/testsController.js
+++ controllers/testsController.js
@@ -1,69 +1,126 @@
 const { Op, literal } = require("sequelize");
-const moment = require('moment');
+const moment = require("moment");
 const { printAndSendError } = require("../helpers/responsesOfReq");
 const Grades = require("../models/grades");
 const Gro_cou = require("../models/gro_cou");
 const Stu_gro = require("../models/stu_gro");
 const Test = require("../models/test");
 const getTestInfo = require("../helpers/tests");
+const Course = require("../models/courses");
 
-const assignTestToStudent = async(req, res) => {
-    const {id_student} =  req
-    const {application_date,id_course} = req.body
-    try {
-        const {id_group} = await Stu_gro.findOne({
-            where : {id_student},
-            attributes : ['id_group'],
-            raw : true
-        })
-        const {id_gro_cou} = await Gro_cou.findOne({ 
-            where : {[Op.and]:[{id_group},{id_course}]},
-            attributes : ['id_gro_cou'],
-            raw : true
-        })
-        const {folio:lastFolio} = await Test.findOne({attributes:['folio'],order:[['folio','DESC']],raw:true}) || 1
-        const {id_grade} = await Grades.findOne({
-            where : {[Op.and]:[{id_student},{id_course}]},
-            attributes : ['id_grade'],
-            raw : true
-        })
-        const test = await Test.update({folio : lastFolio + 1,type:'Extraordinario',application_date,assigned_test_date : moment().format('YYYY-MM-DD'),applied:false},{where : {id_grade}})
-        return res.json({
-            ok : true,
-            msg : 'Examen asignado correctamente.'
-        });
-    } catch (error) {
-        printAndSendError(res,error)
-    }
-}
+const assignTestToStudent = async (req, res) => {
+  const { id_student } = req;
+  const { application_date, id_course } = req.body;
+  try {
+    const { id_group } = await Stu_gro.findOne({
+      where: { id_student },
+      attributes: ["id_group"],
+      raw: true,
+    });
+    const { id_gro_cou } = await Gro_cou.findOne({
+      where: { [Op.and]: [{ id_group }, { id_course }] },
+      attributes: ["id_gro_cou"],
+      raw: true,
+    });
+    const { folio: lastFolio } =
+      (await Test.findOne({
+        attributes: ["folio"],
+        order: [["folio", "DESC"]],
+        raw: true,
+      })) || 1;
+    const { id_grade } = await Grades.findOne({
+      where: { [Op.and]: [{ id_student }, { id_course }] },
+      attributes: ["id_grade"],
+      raw: true,
+    });
+    const test = await Test.update(
+      {
+        folio: lastFolio + 1,
+        type: "Extraordinario",
+        application_date,
+        assigned_test_date: moment().format("YYYY-MM-DD"),
+        applied: false,
+      },
+      { where: { id_grade } }
+    );
+    return res.json({
+      ok: true,
+      msg: "Examen asignado correctamente.",
+    });
+  } catch (error) {
+    printAndSendError(res, error);
+  }
+};
 
-const getTests = async(req, res) => {
-    const {applied,dateOrder = 'desc'} = req.query
-    const tests = await getTestInfo(false,{...(applied!=undefined)?{applied}:{}},dateOrder)
+const getTests = async (req, res) => {
+  const { applied, dateOrder = "desc" } = req.query;
+  try {
+    const tests = await getTestInfo(
+      false,
+      { ...(applied != undefined ? { applied } : {}) },
+      dateOrder
+    );
     res.json({
-        ok : true,
-        tests
-    })
-}
+      ok: true,
+      tests,
+    });
+  } catch (error) {
+    printAndSendError(res, error);
+  }
+};
 
-const changeApplicationDate = async( req, res) => {
-    const {application_date} = req.body
-    const {id_test} = req.params
-    try {
-        const test = await Test.findByPk(id_test)
-        await test.update({application_date})
-        return res.json({
-            ok : true,
-            msg : `Fecha de aplicación del examen con id ${test.id_test} actualizada correctamente.`
-        })
-    } catch (err) {
-        printAndSendError(res,err)        
+const changeApplicationDate = async (req, res) => {
+  const { application_date } = req.body;
+  const { id_test } = req.params;
+  try {
+    const test = await Test.findByPk(id_test);
+    await test.update({ application_date });
+    return res.json({
+      ok: true,
+      msg: `Fecha de aplicación del examen con id ${test.id_test} actualizada correctamente.`,
+    });
+  } catch (err) {
+    printAndSendError(res, err);
+  }
+};
+
+const getTestStudentCanTake = async (req, res) => {
+  try {
+    const { id_student } = req;
+    Grades.belongsTo(Course, { foreignKey: "id_course" });
+    Course.hasMany(Grades, { foreignKey: "id_course" });
+    let grades = await Grades.findAll({
+      where: {
+        [Op.and]: [
+          { id_student },
+          {
+            [Op.or]: [
+              { grade: "NP" },
+              { grade: { [Op.notIn]: ["7", "8", "9", "10"] } },
+            ],
+          },
+        ],
+      },
+      include: { model: Course },
+    });
+    if (grades.length > 0) {
+      grades = grades.map(({ course: { id_course, course_name } }) => ({
+        id_course,
+        course_name,
+      }));
     }
-}
-
+    return res.json({
+      ok: true,
+      tests: grades,
+    });
+  } catch (error) {
+    printAndSendError(res, error);
+  }
+};
 
 module.exports = {
-    assignTestToStudent,
-    getTests,
-    changeApplicationDate
+  assignTestToStudent,
+  getTests,
+  changeApplicationDate,
+  getTestStudentCanTake,
 };
--- routes/tests.js
+++ routes/tests.js
@@ -1,29 +1,69 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { assignTestToStudent, getTests, changeApplicationDate } = require("../controllers/testsController");
-const { checkStudentExistence, hasStudentTakenCourse } = require("../middlewares/dbValidations");
+const {
+  assignTestToStudent,
+  getTests,
+  changeApplicationDate,
+  getTestStudentCanTake,
+} = require("../controllers/testsController");
+const {
+  checkStudentExistence,
+  hasStudentTakenCourse,
+} = require("../middlewares/dbValidations");
 const validateJWT = require("../middlewares/validar-jwt");
 const { validateFields } = require("../middlewares/validateFields");
 
 const testRouter = Router();
 
-testRouter.post('/',[
+testRouter.get(
+  "/student/:matricula",
+  [
+    check("matricula", "La matricula del estudiante es obligatoria.")
+      .notEmpty()
+      .isLength({ max: 15 }),
+    validateFields,
     validateJWT,
-    check('matricula','La matricula del estudiante es obligatoria.').notEmpty().isLength({max:15}),
-    check('id_course','El id del curso es un número y es obligatorio.').notEmpty().isLength({max:15}),
-    check('application_date','La fecha de aplicación del examen es obligatoria y valida.').notEmpty().isLength({max:15}),
+    checkStudentExistence,
+  ],
+  getTestStudentCanTake
+);
+
+testRouter.post(
+  "/",
+  [
+    validateJWT,
+    check("matricula", "La matricula del estudiante es obligatoria.")
+      .notEmpty()
+      .isLength({ max: 15 }),
+    check("id_course", "El id del curso es un número y es obligatorio.")
+      .notEmpty()
+      .isLength({ max: 15 }),
+    check(
+      "application_date",
+      "La fecha de aplicación del examen es obligatoria y valida."
+    )
+      .notEmpty()
+      .isLength({ max: 15 }),
     validateFields,
     checkStudentExistence,
-    hasStudentTakenCourse
-],assignTestToStudent)
+    hasStudentTakenCourse,
+  ],
+  assignTestToStudent
+);
 
-testRouter.get('/',[
-    validateJWT,
-],getTests)
+testRouter.get("/", [validateJWT], getTests);
 
-testRouter.put('/:id_test',[
-    check('application_date','La nueva fecha de aplicación del exámen es obligatoria y debe de ser valida').isDate(),
+testRouter.put(
+  "/:id_test",
+  [
+    check(
+      "application_date",
+      "La nueva fecha de aplicación del exámen es obligatoria y debe de ser valida"
+    ).isDate(),
     validateFields,
     validateJWT,
-],changeApplicationDate)
+  ],
+  changeApplicationDate
+);
+
 module.exports = testRouter;
